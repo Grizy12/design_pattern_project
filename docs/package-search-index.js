5ab5a3430884a02e4caec0676406532eaf8ce0f0
Grizy12 <104630982+Grizy12@users.noreply.github.com>
2025-12-01 22:38:19 +0100

    Add files via upload

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"control"},{"l":"control.command"},{"l":"control.maillon"},{"l":"control.state"},{"l":"model"},{"l":"vue"}]
